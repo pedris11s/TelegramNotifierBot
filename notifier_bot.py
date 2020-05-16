cf98e55536c39fee7a3f1f23e4a570f827453973
Pedro Torres Paredes <pedris11s.dev@gmail.com>
2020-05-15 23:08:09 -0400

    sending new users to api

--- notifier_bot.py
+++ notifier_bot.py
@@ -1,12 +1,27 @@
-import os
+import os, json
+import requests
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+ADD_USER_API_URL = 'https://notifier-bot-api.herokuapp.com/User'
+
 def start(update, context):
-    update.message.reply_text('Welcome...')
+    payload = {
+        "UserId": int(update.message.chat.id),
+        "Username": str(update.message.chat.username),
+        "FirstName": str(update.message.chat.first_name)
+    }
+
+    print(payload)
+
+    response = requests.post(ADD_USER_API_URL,  data=json.dumps(payload), headers={"Content-Type":"application/json"})
+
+    print(response.json())
+
+    update.message.reply_text('Bienvenido...ahora puede recibir notificaciones de sus canales. Consulte la API https://notifier-bot-api.herokuapp.com/swagger')
 
 
 def echo(update, context):
-    update.message.reply_text(update.message.text)
+    update.message.reply_text('Mensaje recibido...')
 
 
 def main():
@@ -15,11 +30,8 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-
     dp.add_handler(MessageHandler(Filters.text, echo))
-
     updater.start_polling()
-
     updater.idle()
 
 if __name__ == '__main__':
